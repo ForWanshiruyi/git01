87d82b1f6ca1297bbca17f6ce4776ad20bbc6052
wanshiruyi <wanshiruyi.com>
2022-07-05 21:27:53 +0800

    03-commit

--- src/com/mystudy/Git01.java
+++ src/com/mystudy/Git01.java
@@ -4,13 +4,11 @@ package com.mystudy;
  * @author :万事如意
  * @date : 2022/7/5 20:46
  */
-/*VCS==import into version==create git repository通过这些操作可以在
-* 提示的文件夹下生成本地库即.git文件，不要自己指定文件夹，否则会报错
-* 生成本地库后是红色，添加进入暂存区后是绿色，体检至本地库后是蓝色
-* 查看版本信息：点击Git==点击Log*/
 public class Git01 {
     public static void main(String[] args) {
         System.out.println("000");
         System.out.println("111");
+        System.out.println("222");
+
     }
 }
